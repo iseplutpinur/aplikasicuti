5898bf4f68a54f92a1af7de4e9d2e307cba22c1e
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2022-01-19 11:30:39 +0700

    fix database

--- dist/config/koneksi.php
+++ dist/config/koneksi.php
@@ -7,6 +7,3 @@ $dbpass = "almardiy_aplikasicuti";
 $dbname = "almardiy_aplikasicuti";
 
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-if ($conn) {
-  die("Tidak dapat terhubung ke database: " . mysqli_error($conn));
-}
